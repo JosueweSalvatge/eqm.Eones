252f4aad7289c503e449be43f06a133b71aecfbe
Maria Magdalena <mcladera5@a.iescanpeublanc.cat>
2023-11-21 10:33:47 +0100

    URLs arrgladas

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from .views import lista_alumnos, detalle_alumno, nueva_visita, lista_visitas
 
 urlpatterns = [
-    path('alumnos/', lista_alumnos, name='lista_alumnos'),
-    path('alumnos/<int:num_exp>/', detalle_alumno, name='detalle_alumno'),
-    path('visitas/nueva/', nueva_visita, name='nueva_visita'),
-    path('visitas/', lista_visitas, name='lista_visitas'),
+    path('admin/', admin.site.urls),
+    path('', include('aplicacion.urls')),
 ]
 
 
