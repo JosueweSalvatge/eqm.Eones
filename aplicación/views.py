425b5b96947c056f5dc9948333d66ada8bc62a48
marianpaye <mbennassar2@a.iescanpeublanc.cat>
2023-11-21 10:33:38 +0100

    base.html

--- aplicación/views.py
+++ aplicación/views.py
@@ -11,7 +11,7 @@ def detalle_alumno(request, num_exp):
     alumno = get_object_or_404(Persona, num_exp=num_exp)
 
     # Pasar la lista de alumnos al template
-    return render(request, 'lista_alumnos.html', {'alumno': alumno})
+    return render(request, 'detalle_alumno.html', {'alumno': alumno})
 
 def nueva_visita(request):
     pass
