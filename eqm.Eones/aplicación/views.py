e1faf8edc1e2438b22a8d57f82d7a16a03763fe6
Sebas Bennassar <sbennassar@a.iescanpeubanc.cat>
2023-11-23 11:58:35 +0100

    intentp vistas

--- eqm.Eones/aplicación/views.py
+++ eqm.Eones/aplicación/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Persona, Visitas
+from .models import Persona, Visita
 from django.shortcuts import render, get_object_or_404
 from datetime import datetime, timedelta
 
