f4a2550798a6b4490d60c6ef78c33fb46353990e
Sebas Bennassar <sbennassar@a.iescanpeubanc.cat>
2023-11-27 09:26:48 +0100

    3r intento fallido de views

--- eqm.Eones/aplicación/views.py
+++ eqm.Eones/aplicación/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-from .models import Persona, Visita
+from .models import Persona, Visitas
 from django.shortcuts import render, get_object_or_404
 from datetime import datetime, timedelta
-from django import os
-from django import time
 
-def is_access_allowed():
-    access_interval = int(os.getenv('ACCESS_INTERVAL', 7200))
-    last_access_time = int(os.getenv('LAST_ACCESS_TIME', 0))
 
-    current_time = int(time.time())
-
-    if current_time - last_access_time >= access_interval:
-        os.environ['LAST_ACCESS_TIME'] = str(current_time)
-        return True
-    else:
-        return False
-
-def lista_Persona(request):
+def lista_alumno(request):
     pass
 
-def detalle_Persona(request, num_exp):
+def detalle_alumno(request, num_exp):
      # Obtener la lista de todos los alumnos desde la base de datos
     Persona = get_object_or_404(Persona, num_exp=num_exp)
 
@@ -28,23 +15,27 @@ def detalle_Persona(request, num_exp):
     return render(request, 'detalle_alumno.html', {'alumno': Persona})
 
 def nueva_visita(request):
-    pass
+    try:
+        tiempo_anterior = datetime()  # Formato: Año, Mes, Día, Hora, Minuto, Segundo
+    except:
+         tiempo_anterior = datetime(2001, 1, 1, 1, 1, 1)
+    tiempo_actual = datetime.now()  # Tiempo actual
+    diferencia = tiempo_actual - tiempo_anterior
     if Persona.VIP and Persona.acompanado:
-        if is_access_allowed:
+        if diferencia > timedelta(hours=2):
             return render(request, 'vip_acompanado_pantalla_naranja.html', {'Persona': Persona})
         else:
             return render(request, 'vip_acompanado_pantalla_verde.html', {'Persona': Persona})
     elif Persona.VIP:
         return render(request, 'vip_pantalla_verde.html', {'Persona': Persona})
     elif Persona.acompanado:
-        if is_access_allowed:
+        if diferencia > timedelta(hours=2):
             return render(request, 'acompanado_pantalla_naranja.html', {'Persona': Persona})
         else:
             return render(request, 'acompanado_pantalla_roja.html', {'Persona': Persona})
     else:
-        if is_access_allowed:
+        if diferencia > timedelta(hours=2):
             return render(request, 'comunes_pantalla_verde.html', {'Persona': Persona})
         else:
             return render(request, 'comunes_pantalla_roja.html', {'Persona': Persona})
-def lista_visitas(request):
-    pass
+        
