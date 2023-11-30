05f3ecc2da112a297436c020a7fcf9725ed3f930
Sebas Bennassar <sbennassar@a.iescanpeubanc.cat>
2023-11-30 11:37:51 +0100

    6t intento fallido de views

--- eqm.Eones/aplicación/views.py
+++ eqm.Eones/aplicación/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Persona, Visitas
+from django import Persona, Visitas
 from django.shortcuts import render, get_object_or_404
 from datetime import datetime, timedelta
 
